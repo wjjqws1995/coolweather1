01c0e63022886b4b435e9286293e37bb3b953744
unknown <680755@W68-680755.internet.com>
2017-12-15 15:26:34 +0800

    增加后台更新功能

--- app/src/main/java/com/example/a680755/coolweather1/WeatherActivity.java
+++ app/src/main/java/com/example/a680755/coolweather1/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.a680755.coolweather1;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.example.a680755.coolweather1.gson.Forecast;
 import com.example.a680755.coolweather1.gson.Weather;
+import com.example.a680755.coolweather1.service.AutoUpdateService;
 import com.example.a680755.coolweather1.util.HttpUtil;
 import com.example.a680755.coolweather1.util.Utility;
 
@@ -198,5 +200,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherlayout.setVisibility(View.VISIBLE);
+        Intent intent =new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
